487bcd12bd2694cc46272c75c395838a02386351
Arielly09 <162440483+Arielly09@users.noreply.github.com>
2024-04-09 10:10:17 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,24 +1,45 @@
 import os
 
-print ("""Sabor Express
+restaurante = ['Pizza','Xp']
+
+def exibir_npme_do_programa():
+ print ("""Paula Tejano
        """)
-
-print('1. Cadastrar restaurante')
-print('2.Listar restaurante')
-print('3.Ativar restaurante')
-print('4.Sair')
-
-opcao_escolhida=int(input('Escolha uma opção:'))
+def exibir_opcoes():
+ print('1. Cadastrar restaurante')
+ print('2.Listar restaurante')
+ print('3.Ativar restaurante')
+ print('4.Sair')
 
 def finalizar_app():
-    os.system('clear')
-    print('Encerrando o programa\n')
+   exibir_subtitulo('Finalizar App')
 
-if opcao_escolhida ==1:
-     print('Cadatrar restaurante')
-elif opcao_escolhida ==2:
-      print('Listar restaurante')
-elif opcao_escolhida == 3:
-      print('Ativa restaurante')
-else: 
-     finalizar_app()
+def voltar_ao_menu_principal():
+    input('\n Digite a tecla "Enter"para voltar ao menu principal')
+    main()
+
+opcao_escolhida=int(input('Escolha uma opção:'))
+def exibir_subtitulo(texto):
+    os.system('clear')
+    print('texto')
+    print()
+
+def escolher_opcao():
+ 
+      if opcao_escolhida ==1:
+            print('Cadatrar restaurante')
+      elif opcao_escolhida ==2:
+            print('Listar restaurante')
+      elif opcao_escolhida == 3:
+            print('Ativa restaurante')
+      else: 
+            finalizar_app()
+
+def main():
+         os.system('clear')
+         exibir_npme_do_programa()
+         exibir_opcoes()
+         escolher_opcao()
+
+if __name__== '__main__':
+    main()
