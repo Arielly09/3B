0a7803c4f68653a6ce7091f319f305fc6649ba12
Arielly09 <162440483+Arielly09@users.noreply.github.com>
2024-04-11 08:44:02 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,39 +1,75 @@
 import os
 
-restaurante = ['Pizza','Xp']
+restaurantes = ['Pizza','Xp']
 
 def exibir_npme_do_programa():
- print ("""Paula Tejano
+ print ("""
+       Paula Tejano
        """)
+
 def exibir_opcoes():
  print('1. Cadastrar restaurante')
  print('2.Listar restaurante')
  print('3.Ativar restaurante')
  print('4.Sair')
 
+
 def finalizar_app():
    exibir_subtitulo('Finalizar App')
+
 
 def voltar_ao_menu_principal():
     input('\n Digite a tecla "Enter"para voltar ao menu principal')
     main()
 
-opcao_escolhida=int(input('Escolha uma opção:'))
+
+
 def exibir_subtitulo(texto):
     os.system('clear')
-    print('texto')
-    print()
+    print('Encerrando o programa\n')
+
+
 
 def escolher_opcao():
- 
-      if opcao_escolhida ==1:
-            print('Cadatrar restaurante')
-      elif opcao_escolhida ==2:
-            print('Listar restaurante')
-      elif opcao_escolhida == 3:
-            print('Ativa restaurante')
-      else: 
-            finalizar_app()
+ try:
+
+    opcao_escolhida = int(input('Escolha uma opção'))
+
+    if opcao_escolhida == 1:
+        cadastro_novo_restaurante()
+    elif opcao_escolhida == 2:
+        listar_restaurante
+    elif opcao_escolhida == 3:
+        print('Ativa restaurante')
+    elif opcao_escolhida == 4:        
+        finalizar_app()
+    else:
+       opcao_invalida() 
+ except:
+      opcao_invalida()
+
+def opcao_invalida():
+    print('opção invalida!\n')
+    voltar_ao_menu_principal()
+
+def listar_restaurante():
+    exibir_subtitulo('Listando os restaurantes')
+
+    for restaurante in restaurantes:
+        print(f'*{restaurante}')
+
+    voltar_ao_menu_principal
+
+def exibir_subtitulo(texto):
+    os.system('clear')
+    print(texto)
+    print()
+def cadastro_novo_restaurante():
+    exibir_subtitulo('Cadastro de novos restaurantes: ')
+    nome_restaurante = input('Digite o nome do novo restaurante')
+    restaurantes.append(nome_restaurante)
+    print(f'O restaurante{nome_restaurante}foi cadastrado com sucesso!')
+    voltar_ao_menu_principal() 
 
 def main():
          os.system('clear')
